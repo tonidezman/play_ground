ce90e1bc4961feca4994dbe8e41071f7c762b983
Toni Dezman <toni.dezman@gmail.com>
2018-02-09 13:43:15 +0100

    adding new pages works

--- rails/simple_cms2/app/controllers/admin/pages_controller.rb
+++ rails/simple_cms2/app/controllers/admin/pages_controller.rb
@@ -7,8 +7,28 @@ class Admin::PagesController < ApplicationController
   end
 
   def new
+    @subject = Subject.find(params[:subject_id])
+    @page = Page.new
+  end
+
+  def create
+    @subject = Subject.find(params[:subject_id])
+    @page = Page.new(page_params)
+    @page.subject = @subject
+
+    if @page.save
+      redirect_to admin_subject_pages_path(@subject), notice: "Page (#{@page.name}) successfuly created"
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:name, :subject_id)
   end
 end
