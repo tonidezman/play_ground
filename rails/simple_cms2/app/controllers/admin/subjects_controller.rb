c2e78dd61431094fb7331e9260cbb18e208060d8
Toni Dezman <toni.dezman@gmail.com>
2018-02-09 12:39:31 +0100

    adds subject controller and views

--- rails/simple_cms2/app/controllers/admin/subjects_controller.rb
+++ rails/simple_cms2/app/controllers/admin/subjects_controller.rb
@@ -1,13 +1,47 @@
 class Admin::SubjectsController < ApplicationController
+  layout 'admin'
+
   def index
+    @subjects = Subject.all.limit(10)
   end
 
   def show
   end
 
   def new
+    @subject = Subject.new(name: "default name")
+  end
+
+  def create
+    if Subject.create(subject_params)
+      redirect_to admin_subjects_path, notice: "Subject was successfuly created!"
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(subject_params)
+      redirect_to admin_subjects_path, notice: "Subject was successfuly updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    subject = Subject.find(params[:id])
+    subject.destroy
+    redirect_to admin_subjects_path, notice: "Subject #{subject.name} was successfuly deleted!"
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name)
   end
 end
--- rails/simple_cms2/app/helpers/application_helper.rb
+++ rails/simple_cms2/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def display_flash(css_class, msg)
+        if css_class == "notice"
+            css = "bg-success"
+        elsif css_class == "error"
+            css = "bg-danger"
+        end
+        "<p class='#{css}'>#{msg}</p>"
+    end
 end
