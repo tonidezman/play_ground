455ed1cc77b6daa716f93bed698f516d5f4a8183
Toni Dezman <toni.dezman@gmail.com>
2018-02-10 07:50:12 +0100

    tags controller, views

---
+++ rails/simple_cms2/app/controllers/admin/tags_controller.rb
@@ -0,0 +1,7 @@
+class Admin::TagsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ rails/simple_cms2/test/controllers/admin/tags_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class Admin::TagsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get admin_tags_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get admin_tags_edit_url
+    assert_response :success
+  end
+
+end
