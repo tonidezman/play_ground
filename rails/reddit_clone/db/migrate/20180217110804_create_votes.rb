b66d71f4ea7db8b3b4326dcdedccac95d4ba8a81
Toni Dezman <toni.dezman@gmail.com>
2018-02-17 12:41:37 +0100

    adds category migration and changes votes migration to add index

--- rails/reddit_clone/db/migrate/20180217110804_create_votes.rb
+++ rails/reddit_clone/db/migrate/20180217110804_create_votes.rb
@@ -2,10 +2,10 @@ class CreateVotes < ActiveRecord::Migration[5.1]
   def change
     create_table :votes do |t|
       t.string :votable_type
+      t.integer :votable_id
       t.integer :user_id, index: true
-      t.integer :votable_id, index: true
-
       t.timestamps
     end
+    add_index :votes, [:votable_type, :votable_id]
   end
 end
