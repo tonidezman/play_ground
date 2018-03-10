1d3f839a6c992246d4a47afc3c13db31b1973e4a
Toni Dezman <toni.dezman@gmail.com>
2018-03-10 13:14:04 +0100

    changes to Redit clone app

--- rails/reddit_clone/app/models/user.rb
+++ rails/reddit_clone/app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def my_virtual_attribute
+    puts "hello"
+  end
+
+  def my_virtual_attribute=(msg)
+    puts "message ===> #{msg}"
+  end
 end
