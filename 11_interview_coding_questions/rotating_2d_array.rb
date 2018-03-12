3bd09411a2a6e6935ae5c9eff5faaeec55936b6a
Toni Dezman <toni.dezman@gmail.com>
2018-03-12 05:41:13 +0100

    updates "rotating 2d array" solution

--- 11_interview_coding_questions/rotating_2d_array.rb
+++ 11_interview_coding_questions/rotating_2d_array.rb
@@ -1,20 +1,25 @@
-def rotate_arr(arr_of_arrays)
-
-  result = Array.new(arr_of_arrays.length) {[]}
-
-  arr_of_arrays.each_with_index do |arr, i|
-    arr.each_with_index do |el, j|
-      result[j].unshift(el)
+def rotate2d_array(arr)
+  dup_array = arr.map(&:dup)
+  arr.each_with_index do |row, i|
+    row.each_with_index do |num, j|
+      row, col = new_coordinates(arr.length, i, j)
+      dup_array[row][col] = num
     end
   end
 
-  result
+  dup_array
 end
 
-x = [
+def new_coordinates(n, i, j)
+  new_i = j
+  new_j = n - 1 - i
+  [new_i, new_j]
+end
+
+fields = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
 ]
 
-puts rotate_arr(x).inspect
+puts rotate2d_array(fields).inspect
