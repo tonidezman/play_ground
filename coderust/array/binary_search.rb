f65bbaab316c53cd0aa8da46e53b027db74f0b3b
Toni Dezman <toni.dezman@gmail.com>
2018-04-04 18:26:56 +0200

    solution for binary_search challenge

---
+++ coderust/array/binary_search.rb
@@ -0,0 +1,32 @@
+# find number (ie 20)
+# return -1 if number is not found
+# return index of the number found
+# array is sorted in ascending order
+
+sorted_nums = [1, 10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155, 162, 176, 188, 199, 200, 210, 222]
+
+def binary_search(sorted_nums, search_num)
+  not_found_flag = -1
+  return not_found_flag if (search_num < sorted_nums.first) || (search_num > sorted_nums.last)
+
+  left_pointer = 0
+  right_pointer = sorted_nums.length - 1
+  return left_pointer if search_num == sorted_nums[left_pointer]
+
+  while left_pointer < right_pointer
+    middle_count_between_left_and_right_pointer = ((right_pointer - left_pointer).to_f / 2).ceil
+    guess_index = left_pointer + middle_count_between_left_and_right_pointer
+    guess_num = sorted_nums[guess_index]
+
+    if guess_num == search_num
+      return guess_index
+    elsif guess_num < search_num
+      left_pointer = guess_index
+    elsif guess_num > search_num
+      right_pointer = guess_index
+    end
+  end
+  return not_found_flag
+end
+
+puts binary_search(sorted_nums, 1)
