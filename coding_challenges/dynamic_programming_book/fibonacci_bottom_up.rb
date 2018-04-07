12f9434c0dedfa1a0dc690c53a1f34106de31100
Toni Dezman <toni.dezman@gmail.com>
2018-04-07 19:45:23 +0200

    adds cached version of bottom up fib solution

--- coding_challenges/dynamic_programming_book/fibonacci_bottom_up.rb
+++ coding_challenges/dynamic_programming_book/fibonacci_bottom_up.rb
@@ -1,12 +1,24 @@
-def fib(wanted_fib)
-  return wanted_fib if wanted_fib < 2
+# bottom up calculating fibonacci number
+class Fib
+  attr_accessor :memoize
 
-  current_fib, prev_fib, next_fib = [2, 1, 1] # rubocop:disable all
-  while current_fib < wanted_fib
-    temp = next_fib
-    next_fib = prev_fib + next_fib
-    prev_fib = temp
-    current_fib += 1
+  def initialize
+    @memoize = [0, 1]
   end
-  next_fib
+
+  def calculate(wanted_fib) #rubocop:disable all
+    return wanted_fib if wanted_fib < 2
+    return memoize[wanted_fib] if memoize[wanted_fib]
+
+    len = memoize.length - 1
+    current_fib, prev_fib, next_fib = [len, memoize[len-1], memoize[len]] # rubocop:disable all
+    while current_fib < wanted_fib
+      temp = next_fib
+      next_fib = prev_fib + next_fib
+      memoize[current_fib + 1] = next_fib
+      prev_fib = temp
+      current_fib += 1
+    end
+    next_fib
+  end
 end
