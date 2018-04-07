5d5148d6f9109b201d689ed4f8ad9146698c55bd
Toni Dezman <toni.dezman@gmail.com>
2018-04-07 17:42:09 +0200

    adds memoization

--- coding_challenges/dynamic_programming_book/fibonacci.rb
+++ coding_challenges/dynamic_programming_book/fibonacci.rb
@@ -1,5 +1,11 @@
 def fib(num)
+  memoize = {}
+
   return 1 if [1, 2].include? num
   return num if num < 2
-  fibonacci(num - 1) + fibonacci(num - 2)
+  return memoize[num] && puts("pulling from cache #{num}") if memoize[num]
+  calculated_fibonacci = fibonacci(num - 1) + fibonacci(num - 2)
+  memoize[num] = calculated_fibonacci
+  puts("calculating and caching => #{num}")
+  calculated_fibonacci
 end
