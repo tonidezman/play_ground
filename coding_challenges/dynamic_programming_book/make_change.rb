f5821122cb901c00502418cc18077817956c8cc2
Toni Dezman <toni.dezman@gmail.com>
2018-04-08 11:14:28 +0200

    brut force making change solution

--- coding_challenges/dynamic_programming_book/make_change.rb
+++ coding_challenges/dynamic_programming_book/make_change.rb
@@ -1,34 +1,19 @@
-# class for counting how many coins for certain number of cash
-class Casher
-  attr_reader :coins, :cash
-  attr_accessor :min_coins
+COINS = [10, 6, 1]
+$current_min = Float::INFINITY
 
-  def initialize(coins, cash)
-    @coins = coins.sort { |a, b| b - a }
-    @cash = cash
-    @min_coins = Float::INFINITY #rubocop:disable all
+def make_change(cash, coin_counter = 0) #rubocop:disable all
+  if cash.zero?
+    if coin_counter < $current_min
+      $current_min = coin_counter
+    end
+    return 0
   end
 
-  def count_change
-    _count_change(coins, cash)
-    puts min_coins
-  end
-
-  def _count_change(coins, current_cash, coin_counter = 0)
-    if current_cash.zero? && coin_counter < min_coins
-      self.min_coins = coin_counter
-    elsif current_cash < 0
-      return
-    end
-
-    return coin_counter if current_cash.zero?
-    return Float::INFINITY if current_cash < 0
-
-    coins.reverse.each do |coin|
-      _count_change(coins, current_cash - coin, coin_counter += 1)
-    end
+  COINS.each do |coin|
+    remaining_coins = cash - coin
+    remaining_coins >= 0 && make_change(remaining_coins, coin_counter += 1)
   end
 end
 
-coins = [1, 5, 10, 25]
-puts Casher.new(coins, 6).count_change
+make_change(11)
+puts $current_min
