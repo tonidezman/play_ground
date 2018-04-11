5170db66665fae9ddd552f15cc847389899da62e
Toni Dezman <toni.dezman@gmail.com>
2018-04-11 17:43:50 +0200

    Learning golang initial commit

--- coding_challenges/coderust/array/max_single_sale_profit.rb
+++ coding_challenges/coderust/array/max_single_sale_profit.rb
@@ -1,21 +1,11 @@
 def max_profit(arr)
   current_max = -Float::INFINITY
 
-  arr.each_with_index do |buy_value, i|
-    is_last_index = (i+1) == arr.length
-    break if is_last_index
-    (i + 1).upto(arr.length-1) do |j|
-      sell_value = arr[j]
 
-      current_profit = sell_value - buy_value
-      if current_profit > current_max
-        current_max = current_profit
-      end
-    end
-  end
 
-  current_max
+
 end
 
-arr = [8, 5, 12, 9, 19, 1]
+# arr = [8, 5, 12, 9, 19, 1]
+arr = [21, 12, 11, 9, 6, 3]
 p max_profit(arr)
