ad644a2bbe4b7ae2adb332186c4b791f95265068
Toni Dezman <toni.dezman@gmail.com>
2018-04-12 05:30:33 +0200

    solution to max single sale profit

--- coding_challenges/coderust/array/max_single_sale_profit.rb
+++ coding_challenges/coderust/array/max_single_sale_profit.rb
@@ -1,9 +1,25 @@
 def max_profit(arr)
-  current_max = -Float::INFINITY
+  if arr.length < 2
+    raise ArgumentError.new("There have to be at least two values")
+  end
 
+  current_buy = arr[0]
+  current_sell = arr[1]
+  current_profit = current_sell - current_buy
 
+# arr = [21, 12, 11, 9, 6, 3]
+  i = 2
+  while i < arr.length
+    prev_price = arr[i-1]
+    if prev_price < current_buy
+      current_buy = prev_price
+    end
+    sell_price = arr[i]
+    current_profit = [current_profit, sell_price -current_buy].max
+    i += 1
+  end
 
-
+  current_profit
 end
 
 # arr = [8, 5, 12, 9, 19, 1]
